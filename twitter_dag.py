9723d6c42f300d428e85d4759db2ad4aa86b62fb
Swattik Maiti <swattik6979@gmail.com>
2023-04-02 03:28:33 +0530

    Finished airflow dag

--- twitter_dag.py
+++ twitter_dag.py
@@ -6,15 +6,27 @@ from twitter_etl import run_twitter_etl
 
 
 
-default_args ={
+default_args = {
     'owner':'airflow',
     'depends_on_past': False,
-    'start_date': datetime(yyyy,m,d),
-    'email':['abc@gmail.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
+    'start_date': datetime(2023,4,2),
+    'email':['swattik6979@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
     'retries': 1,
     'retry_delay': timedelta(minutes=1)
 }
 
+dag = DAG(
+    'twitter_dag',
+    default_args=default_args,
+    description='Twitter ETL Code'
+)
 
+run_etl = PythonOperator(
+    task_id='complete_twitter_etl',
+    python_callable=run_twitter_etl,
+    dag=dag
+)
+
+run_etl
